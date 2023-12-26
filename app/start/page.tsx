4dfdc71e19b26e83638eefd4629acc16a8e2c16c
givvemee <givvemeee@gmail.com>
2023-12-26 19:08:00 +0900

    [feat] 디코딩 코드 추가

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { useStore } from '@/store';
+import { QuizListType } from '@/store/type';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { Button } from '../atoms/button';
 import { Text } from '../atoms/text';
-import { ButtonBox, Container, TextBox } from './styles';
+import { ButtonBox, Container, TextBox, Title } from './styles';
 
 const StartQuiz = () => {
   const { push } = useRouter();
@@ -23,7 +24,24 @@ const StartQuiz = () => {
         const response = await axios.get(
           'https://opentdb.com/api.php?amount=10&type=multiple'
         );
-        setQuizList(response.data.results);
+        const decodedResults = response.data.results.map(
+          (item: QuizListType) => {
+            const parser = new DOMParser();
+            const decodedQuestion = parser.parseFromString(
+              item.question,
+              'text/html'
+            ).body.textContent;
+
+            return {
+              ...item,
+              question: decodedQuestion,
+            };
+          }
+        );
+
+        setQuizList(decodedResults);
+        const storedQuizList = JSON.stringify(decodedResults);
+        localStorage.setItem('quizList', storedQuizList);
       } catch (error) {
         console.error('Error fetching quiz data:', error);
       }
@@ -34,9 +52,9 @@ const StartQuiz = () => {
   return (
     <Container>
       <TextBox>
-        <Text type={'title'} isBold={'true'}>
+        <Title isBold={'true'} align={'center'}>
           퀴즈
-        </Text>
+        </Title>
         <Text>퀴즈는 총 10문항으로 구성되어 있습니다.</Text>
       </TextBox>
       <ButtonBox>
--- app/start/styles.ts
+++ app/start/styles.ts
@@ -1,5 +1,6 @@
 import { styled } from '@/styles/stitches.config';
 import { FlexBox } from '../atoms/FlexBox';
+import { Text } from '../atoms/text';
 
 export const Container = styled(FlexBox, {
   flexDirection: 'column',
@@ -18,4 +19,9 @@ export const TextBox = styled('div', {
 
 export const ButtonBox = styled('div', {
   marginTop: '50px',
+});
+
+export const Title = styled(Text, {
+  fontSize: '30px',
+  marginBottom: '10px',
 });
